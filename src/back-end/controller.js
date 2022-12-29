15d5746e8a9fddaf8f5426fcb290d9fbb5f9b7b3
Fulxe <muruuuu8@gmail.com>
2022-12-29 17:18:23 +0800

    db-updates

--- src/back-end/controller.js
+++ src/back-end/controller.js
@@ -10,39 +10,50 @@ exports.createUser = async (req, res, next) => {
     return;
   }
 
-  const createUser = await UserModule.create({ ...req.body });
+  const createUser = await UserModel.create({ ...req.body });
   res.status(201).json({ message: ` New user is created`, data: createUser });
+};
+exports.getUSers = async (req, res, next) => {
+  try {
+    const user = await UserModul.find();
+    res.status(200).json({
+      message: true,
+      data: users,
+    });
+  } catch (error) {
+    return res.status(400).json({ message: true, data: null });
+  }
 };
 exports.getUser = async (req, res, next) => {
-  if (!req.body?.firstName || !req.body?.email || !req.body?.password) {
-    res
-      .status(400)
-      .json({ message: `Firsname , email or password are required` });
-    return;
+  const { id } = req.params;
+  try {
+    const user = await UserModel.findById(id);
+    res.status(200).json({ message: true, data: post });
+  } catch (error) {
+    return res.status(400).json({ message: true, data: null });
   }
-
-  const createUser = await UserModule.create({ ...req.body });
-  res.status(201).json({ message: ` New user is created`, data: createUser });
 };
 exports.updateUser = async (req, res, next) => {
-  if (!req.body?.firstName || !req.body?.email || !req.body?.password) {
-    res
-      .status(400)
-      .json({ message: `Firsname , email or password are required` });
-    return;
+  const { id } = req.params;
+  try {
+    const user = await UserModel.findByIdAndUpdate(id, { ...req.body });
+    res.status(200).json({
+      message: `User with ${req.params.id} id is updated`,
+      data: post,
+    });
+  } catch (error) {
+    return res.status(400).json({ message: true, data: null });
   }
-
-  const createUser = await UserModule.create({ ...req.body });
-  res.status(201).json({ message: ` New user is created`, data: createUser });
 };
 exports.deleteUser = async (req, res, next) => {
-  if (!req.body?.firstName || !req.body?.email || !req.body?.password) {
-    res
-      .status(400)
-      .json({ message: `Firsname , email or password are required` });
-    return;
+  const { id } = req.params;
+  try {
+    const user = await UserModel.findByIdAndDelete(id, { ...req.body });
+    res.status(200).json({
+      message: `User with ${req.params.id} id is deleted`,
+      data: post,
+    });
+  } catch (error) {
+    return res.status(400).json({ message: true, data: null });
   }
-
-  const createUser = await UserModule.create({ ...req.body });
-  res.status(201).json({ message: ` New user is created`, data: createUser });
 };
--- src/back-end/router.js
+++ src/back-end/router.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const { getUsers } = require("../../../Test/node/controller");
 const router = express.Router();
 
 const { getUser, createUser, updateUser, deleteUser } = require("./controller");
 router
   .get("/", getUser)
   .post("/", createUser)
+  .get("/:id", getUsers)
   .put("/:id", updateUser)
   .delete(":id", deleteUser);
 
